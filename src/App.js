54f13c547cdcea6a3c979b047716929b0424780a
annachuprina <annchuprina166@yandex.ru>
2022-12-06 20:00:12 +0100

    text

--- src/App.js
+++ src/App.js
@@ -17,6 +17,14 @@ function App() {
   return (
         <>
         <li>X: {gyroscope.x}</li>
+        fnblkflnblts: 100% (6/6), done.
+Delta compression using up to 4 threads
+Compressing objects: 100% (4/4), done.
+Writing objects: 100% (4/4), 601 bytes | 300.00 KiB/s, done.
+Total 4 (delta 2), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (2/2), completed with 2 local objects.
+remote: 
+remote: GitHub found 8 vulnerabilities on 
         <Unity style={{border:'1px solid red', maxWidth:'600px', height:'100vh'}}  unityProvider={unityProvider} className=""/>
         </>
   );
