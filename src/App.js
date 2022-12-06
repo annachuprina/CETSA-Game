ba9a5292901990e77ce00e0cb8a5566f5a0a44b6
annachuprina <annchuprina166@yandex.ru>
2022-12-06 19:30:30 +0100

    gyroscope

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import {useRef, useState} from "react"
 import { Unity, useUnityContext } from "react-unity-webgl";
 import logo from './logo.svg';
 import './App.css';
+import useGyroscope from 'react-hook-gyroscope'
 
 
 
 function App() {
+  const gyroscope = useGyroscope()
   const { unityProvider } = useUnityContext({
     loaderUrl: "./webgl1/Build/webgl1.loader.js",
     dataUrl: "./webgl1/Build/webgl1.data",
@@ -13,9 +15,10 @@ function App() {
     codeUrl: "./webgl1/Build/webgl1.wasm",
   })
   return (
-    <div className="App">
-    <Unity style={{border:'1px solid red', maxWidth:'600px', height:'100vh'}}  unityProvider={unityProvider} className=""/>; 
-    </div>
+        <>
+        <li>X: {gyroscope.x}</li>
+        <Unity style={{border:'1px solid red', maxWidth:'600px', height:'100vh'}}  unityProvider={unityProvider} className=""/>
+        </>
   );
 }
 
